f17a9d7351f55c2255031bfb5d450777e0569f35
Leo Xiong <hello@leoxiong.com>
2020-03-02 21:14:15 +1300

    Support single character homoglyphs detection

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,38 @@
+let confusables = {}
+
+chrome.runtime.getPackageDirectoryEntry(function (fs) {
+    fs.getFile('assets/confusables.txt', {type: 'openFile'}, function (fe) {
+        fe.file(function (f) {
+            let reader = new FileReader()
+            reader.onloadend = function (e) {
+                for (let r of e.target.result.matchAll(/^(?<from>[0-9A-F]+)\s*;\s*(?<to>[0-9A-F]+)\s*;.*/gm)) {
+                    if (r) {
+                        let from = String.fromCharCode(parseInt(r.groups['from'], 16))
+                        let to = String.fromCharCode(parseInt(r.groups['to'], 16))
+
+                        if (!(from in confusables)) confusables[from] = []
+                        if (!(to in confusables)) confusables[to] = []
+
+                        confusables[from].push(to)
+                        confusables[to].push(from)
+                    }
+                }
+            }
+            reader.readAsText(f)
+        })
+    })
+})
+
+function isConfusable(a, b) {
+    if (a in confusables) {
+        let homoglyphs = confusables[a]
+        if (homoglyphs.includes(b)) {
+            return true
+        }
+    }
+    return false
+}
+
 let memoize = {}
 
 function levenshteinDistance(s1, s2) {
@@ -7,7 +42,7 @@ function levenshteinDistance(s1, s2) {
     if (s2 === '') return s1.length
 
     let distance
-    if (s1[0] === s2[0]) distance = levenshteinDistance(s1.substr(1), s2.substr(1))
+    if (s1[0] === s2[0] || isConfusable(s1[0], s2[0])) distance = levenshteinDistance(s1.substr(1), s2.substr(1))
     else {
         let l1 = levenshteinDistance(s1, s2.substr(1))
         let l2 = levenshteinDistance(s1.substr(1), s2)
@@ -31,7 +66,6 @@ const knownHosts = [
     'console.cloud.google.com',
     'g.co'
 ].sort((a, b) => b.length - a.length)
-
 
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
     let potential = []
